842d331b254638d9dae6d366c002d2386db7c475
Ben Schlegel <ben5.schlegel@gmail.com>
2023-06-12 11:33:49 +0200

    feat: add device name endpoints

--- plantsense_microcontroller/plantsense_microcontroller.ino
+++ plantsense_microcontroller/plantsense_microcontroller.ino
@@ -62,44 +62,10 @@ double intensity = 0; // between 0.0 and 1 ("brightness percentage")
 // Also used for WiFi access point,
 String device_name = "PlantSense - Planty";
 
-// Currently unused (because not valid), could be replaced in the future
-// const char* ROOT_CA= "-----BEGIN CERTIFICATE-----\n" \
-// "MIIFazCCA1OgAwIBAgIRAIIQz7DSQONZRGPgu2OCiwAwDQYJKoZIhvcNAQELBQAw\n" \
-// "TzELMAkGA1UEBhMCVVMxKTAnBgNVBAoTIEludGVybmV0IFNlY3VyaXR5IFJlc2Vh\n" \
-// "cmNoIEdyb3VwMRUwEwYDVQQDEwxJU1JHIFJvb3QgWDEwHhcNMTUwNjA0MTEwNDM4\n" \
-// "WhcNMzUwNjA0MTEwNDM4WjBPMQswCQYDVQQGEwJVUzEpMCcGA1UEChMgSW50ZXJu\n" \
-// "ZXQgU2VjdXJpdHkgUmVzZWFyY2ggR3JvdXAxFTATBgNVBAMTDElTUkcgUm9vdCBY\n" \
-// "MTCCAiIwDQYJKoZIhvcNAQEBBQADggIPADCCAgoCggIBAK3oJHP0FDfzm54rVygc\n" \
-// "h77ct984kIxuPOZXoHj3dcKi/vVqbvYATyjb3miGbESTtrFj/RQSa78f0uoxmyF+\n" \
-// "0TM8ukj13Xnfs7j/EvEhmkvBioZxaUpmZmyPfjxwv60pIgbz5MDmgK7iS4+3mX6U\n" \
-// "A5/TR5d8mUgjU+g4rk8Kb4Mu0UlXjIB0ttov0DiNewNwIRt18jA8+o+u3dpjq+sW\n" \
-// "T8KOEUt+zwvo/7V3LvSye0rgTBIlDHCNAymg4VMk7BPZ7hm/ELNKjD+Jo2FR3qyH\n" \
-// "B5T0Y3HsLuJvW5iB4YlcNHlsdu87kGJ55tukmi8mxdAQ4Q7e2RCOFvu396j3x+UC\n" \
-// "B5iPNgiV5+I3lg02dZ77DnKxHZu8A/lJBdiB3QW0KtZB6awBdpUKD9jf1b0SHzUv\n" \
-// "KBds0pjBqAlkd25HN7rOrFleaJ1/ctaJxQZBKT5ZPt0m9STJEadao0xAH0ahmbWn\n" \
-// "OlFuhjuefXKnEgV4We0+UXgVCwOPjdAvBbI+e0ocS3MFEvzG6uBQE3xDk3SzynTn\n" \
-// "jh8BCNAw1FtxNrQHusEwMFxIt4I7mKZ9YIqioymCzLq9gwQbooMDQaHWBfEbwrbwv\n" \
-// "qHyGO0aoSCqI3Haadr8faqU9GY/rOPNk3sgrDQoo//fb4hVC1CLQJ13hef4Y53CI\n" \
-// "rU7m2Ys6xt0nUW7/vGT1M0NPAgMBAAGjQjBAMA4GA1UdDwEB/wQEAwIBBjAPBgNV\n" \
-// "HRMBAf8EBTADAQH/MB0GA1UdDgQWBBR5tFnme7bl5AFzgAiIyBpY9umbbjANBgkqv\n" \
-// "hkiG9w0BAQsFAAOCAgEAVR9YqbyyqFDQDLHYGmkgJykIrGF1XIpu+ILlaS/V9lZL\n" \
-// "ubhzEFnTIZd+50xx+7LSYK05qAvqFyFWhfFQDlnrzuBZ6brJFe+GnY+EgPbk6ZGQ\n" \
-// "3BebYhtF8GaV0nxvwuo77x/Py9auJ/GpsMiu/X1+mvoiBOv/2X/qkSsisRcOj/KK\n" \
-// "NFtY2PwByVS5uCbMiogziUwthDyC3+6WVwW6LLv3xLfHTjuCvjHIInNzktHCgKQ5\n" \
-// "ORAzI4JMPJ+GslWYHb4phowim57iaztXOoJwTdwJx4nLCgdNbOhdjsnvzqvHu7Ur\n" \
-// "TkXWStAmzOVyyghqpZXjFaH3pO3JLF+l+/+sKAIuvtd7u+Nxe5AW0wdeRlN8NwdC\n" \
-// "jNPElpzVmbUq4JUagEiuTDkHzsxHpFKVK7q4+63SM1N95R1NbdWhscdCb+ZAJzVc\n" \
-// "oyi3B43njTOQ5yOf+1CceWxG1bQVs5ZufpsMljq4Ui0/1lvh+wjChP4kqKOJ2qxq\n" \
-// "4RgqsahDYVvTH9w7jXbyLeiNdd8XM2w9U/t7y0Ff/9yi0GE44Za4rF2LN9d11TPA\n" \
-// "mRGunUHBcnWEvgJBQl9nJEiU0Zsnvgc/ubhPgXRR4Xq37Z0j4r7g1SgEEzwxA57d\n" \
-// "emyPxgcYxn/eR44/KJ4EBs+lVDR3veyJm+kXQ99b21/+jh5Xos1AnX5iItreGCc=\n" \
-// "-----END CERTIFICATE-----\n";
-
 void setup() {
   Serial.begin(115200);
 
   // Get host from preferences
-
   // setServerHostPreference("https://plantsense.global.rwutscher.com");
   serverHost = getServerHostPreference() + serverPrefix;
   Serial.println(serverHost);
@@ -139,6 +105,8 @@ void setup() {
   server.on("/toggleState", HTTP_POST, handle_toggleState);
   server.on("/setHost", HTTP_POST, handle_setHostAddress);
   server.on("/setCredentials", HTTP_POST, handle_setCredentials);
+  server.on("/name", HTTP_GET, handle_getName);
+  server.on("/name", HTTP_POST, handle_setName);
 
   server.begin();
   Serial.println("HTTP server started!");
@@ -226,12 +194,44 @@ void initAP() {
   WiFi.softAPConfig(IP, IP, NMask);
 }
 
+void SetUpMDNS() {
+  String mac = WiFi.macAddress();
+  mac.replace(":", "-");
+  String deviceHost = HOST_PREFIX + mac;
+  if (MDNS.begin(deviceHost)) {
+    Serial.println(F("mDNS responder started"));
+    Serial.print(F("I am: "));
+    Serial.println(deviceHost);
+
+    // Add service to MDNS-SD
+    MDNS.addService(deviceHost, "tcp", 80);
+  } else {
+    while (1) {
+      Serial.println(F("Error setting up MDNS responder"));
+      delay(1000);
+    }
+  }
+}
+
 // Gets host preference from storage
 String getServerHostPreference() {
   preferences.begin("serverInfo", false);
   String host = preferences.getString("host", "");
   preferences.end();
   return host;
+}
+
+String getDeviceNamePreference() {
+  preferences.begin("deviceInfo", false);
+  String deviceName = preferences.getString("name", "");
+  preferences.end();
+  return deviceName;
+}
+
+void setDeviceNamePreference(String name) {
+  preferences.begin("deviceInfo", false);
+  preferences.putString("name", name);
+  preferences.end();
 }
 
 // Set host preference
@@ -405,7 +405,7 @@ void handle_setCredentials() {
   server.send(200);
 }
 
-// Handle POST ("/setCredentials")
+// Handle POST ("/setHost")
 void handle_setHostAddress() {
   String body = server.arg("plain");
   deserializeJson(multiArgJson, body);
@@ -429,21 +429,29 @@ void handle_toggleState() {
   server.send(200);
 }
 
-void SetUpMDNS() {
-  String mac = WiFi.macAddress();
-  mac.replace(":", "-");
-  String deviceHost = HOST_PREFIX + mac;
-  if (MDNS.begin(deviceHost)) {
-    Serial.println(F("mDNS responder started"));
-    Serial.print(F("I am: "));
-    Serial.println(deviceHost);
+void handle_setName() {
+  String body = server.arg("plain");
+  deserializeJson(multiArgJson, body);
 
-    // Add service to MDNS-SD
-    MDNS.addService(deviceHost, "tcp", 80);
-  } else {
-    while (1) {
-      Serial.println(F("Error setting up MDNS responder"));
-      delay(1000);
-    }
+  if (multiArgJson.containsKey("name")) {
+    // Get value from payload
+    String name = multiArgJson["name"];
+    Serial.println("Name from json: " + name);
+    setDeviceNamePreference(name);
   }
+
+  // Send response
+  server.send(200);
 }
+
+void handle_getName() {
+  StaticJsonDocument<100> nameJson;
+  String prefName = getDeviceNamePreference();
+  Serial.println("Pref name: " + prefName);
+  nameJson["name"] = prefName;
+  String jsonString;
+  serializeJson(nameJson, jsonString);
+  // server.sendContent("test");
+  server.send(200, "text", jsonString);
+}
+
